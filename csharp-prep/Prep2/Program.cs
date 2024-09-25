9fe00e2c6247053fa571d90e0912fab3266029c5
TDashner <das21007@byui.edu>
2024-09-25 14:15:48 -0600

    Homework

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage?  ");
+        string percentage = Console.ReadLine();
+        int number = int.Parse(percentage);
+
+        string letter = "";
+
+        if (number >= 90)
+        {   
+            letter = "A";
+        }
+        else if (number >= 80)
+        {
+            letter = "B";
+        }
+        else if (number >= 70)
+        {
+            letter = "C";
+        }
+        else if (number >= 60)
+        {
+            letter = "D";
+            Console.WriteLine("You did not pass the class. Good luck next time!");
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+
+        if (number >= 70)   
+        {
+            Console.WriteLine("Congradulations, you passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("You did not pass the class. Good luck next time!");
+        } 
     }
 }
