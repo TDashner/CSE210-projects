b59b75ac020d6825999eab1341e4a7a37e7df681
TDashner <das21007@byui.edu>
2024-09-25 19:55:17 -0600

    Parts 3-5 homework

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,35 @@
 using System;
 
 class Program
+
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        List<int> numberList; // OR List<int> numberList = new(); 
+        numberList = new(); // for both of these to be on the same line
+
+        int input = -1;
+        while (input != 0)
+        {
+            Console.Write("Enter something: ");
+            string inputString = Console.ReadLine();
+            input = int.Parse(inputString);
+            if (input != 0)
+            {
+                numberList.Add(input);
+            }
+        }
+
+        foreach (int item in numberList)
+        {
+            Console.WriteLine(item);
+        }
+
+        //List<int> numberList;
+        //These two are the same. You want the first one for a quicker way
+        //numberList = new();
+        //numberList = new List<int>();
+
+
     }
 }
