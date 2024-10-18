2b7fed1ed3eea95547be3ff848ebfc7b6a7ae97e
TDashner <das21007@byui.edu>
2024-10-18 16:00:33 -0600

    Cash Register

---
+++ sandbox/CashRegister/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace CashRegister;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, Cash Register!");
+
+        Bin myBin = new("quarters", 40, (float) 0.25);
+        myBin.ModifyAmount(+6);
+        Console.WriteLine(myBin.TotalValue());
+
+    }
+}
