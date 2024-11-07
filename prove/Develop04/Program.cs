8b86904d58ec488c0701fe4c884929ad71298c65
TDashner <das21007@byui.edu>
2024-11-07 09:51:24 -0700

    starting out

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        
+        while (true)
+        {
+            Console.WriteLine("Which option would you like to choose?");
+            var input = Console.ReadLine();
+
+            if (input == "1")
+                break;
+            if (input == "2")
+                break;
+            if (input == "3")
+                break;
+            if (input == "4")
+                break;
+
+
+
+        }
+
     }
+
+    // while (true)
+    // {
+    //     Console.WriteLine("Press Enter to hide words, or type 'quit' to exit.");
+    //     var input = Console.ReadLine();
+
+    //     if (input?.ToLower() == "quit")
+    //         break;
+
+    //     if (!scripture.HideRandomWords(1))
+    //         break;
+
+    //     Console.Clear();
+    //     Console.WriteLine($"{scripture.Reference}\n{scripture.GetDisplayText()}");
+    // }
+    
 }
+
+
+
+
